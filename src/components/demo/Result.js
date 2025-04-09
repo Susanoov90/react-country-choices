cd2ccc7d796d82a7b82a1e77d73ce6996d20fc50
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-04-09 12:57:09 -0400

    new file:   src/components/demo/Result.js

---
+++ src/components/demo/Result.js
@@ -0,0 +1,12 @@
+export const Result = ({ title, country }) => {
+    return(
+        <div>
+            <div>
+                Title : <strong>{title}</strong>
+            </div>
+            <div>
+                Pays : <strong>{country}</strong>
+            </div>
+        </div>
+    )
+}
